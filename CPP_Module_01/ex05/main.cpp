5a5cbbecff80c06143052bc034eead8ab4c0759c
ilorez <zobirnajdaoui@gmail.com>
2025-09-28 16:44:28 +0100

    my friend harl

---
+++ CPP_Module_01/ex05/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/28 16:26:00 by znajdaou          #+#    #+#             */
+/*   Updated: 2025/09/28 16:42:24 by znajdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl that_one_guy;
+
+    that_one_guy.complain("DEBUG");
+    that_one_guy.complain("INFO");
+    that_one_guy.complain("WARNING");
+    that_one_guy.complain("ERROR");
+    that_one_guy.complain("");
+
+}
