adc729053abe3a7d343477eb79a190693b9e3032
ilorez <zobirnajdaoui@gmail.com>
2025-12-18 19:38:37 +0100

    fix exit leeks error

--- CPP_Module_01/ex01/zombieHorde.cpp
+++ CPP_Module_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 22:26:03 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/11/05 10:59:08 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/12/18 19:37:10 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@
 Zombie* zombieHorde( int N, std::string name )
 {
   if (N <= 0)
-  {
-    exit(EXIT_FAILURE);
-  }
+    return (NULL);
   Zombie *z = new Zombie[N];
   for (int i = 0; i < N; i++)
   {
