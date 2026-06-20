d6fe85fd0ede5fc6555148eac0a30ec8e00aa17f
ilorez <zobirnajdaoui@gmail.com>
2026-06-20 21:44:55 +0100

    start workin on cpp09, leeeeeeets go

---
+++ CPP_Module_09/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "require one arg" << std::endl;
+    return 1;
+  }
+
+
+  return 0;
+}
