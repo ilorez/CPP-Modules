71daea62642ddd0cdaee4a9906fd852bd9266d7e
ilorez <zobirnajdaoui@gmail.com>
2026-06-24 00:54:00 +0100

    updates ex02

---
+++ CPP_Module_09/ex02/includes/PmergeMe.hpp
@@ -0,0 +1,41 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <string>
+#include <utility>
+#include <vector>
+class PmergeMe {
+  private:
+    std::vector<std::pair<int, int> > _pairs;
+    std::vector<int> _input;
+    std::vector<int> _main_chain;
+    std::vector<int> _pend_chain;
+    std::vector<int> _jacobsthal_sq;
+    int _straggler;
+  public:
+    PmergeMe();
+    ~PmergeMe();
+    PmergeMe(const PmergeMe& copy);
+    PmergeMe& operator=(const PmergeMe& copy);
+
+    // member functions
+    void validateInput(const char* sequence);
+    void createPairList();
+    void sortPairs();
+    void setupChains();
+    void generateJacobsthalSq();
+    void insertPends();
+    // debug
+    void printAll();
+
+
+    class PmergeMeException: public std::exception {
+      private:
+        std::string _msg;
+      public:
+        PmergeMeException(std::string msg);
+        ~PmergeMeException() throw();
+        const char* what() const throw();
+    };
+};
+#endif
---
+++ CPP_Module_09/ex02/main.cpp
@@ -0,0 +1,30 @@
+#include "includes/PmergeMe.hpp"
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Error: require positive integer sequence as an argumen!" << std::endl;
+    return (EXIT_FAILURE);
+  }
+  try {
+    PmergeMe re;
+    re.validateInput(argv[1]);
+    re.createPairList();
+    re.sortPairs();
+    re.setupChains();
+    re.generateJacobsthalSq();
+    // use jacon sequence with binary search (lower_bound) to insert number
+    re.insertPends();
+    
+    re.printAll();
+  } catch (std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return (EXIT_FAILURE);
+  }
+  return (EXIT_SUCCESS);
+}
