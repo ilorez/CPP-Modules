3b5734c8e1a846e36b9c675206fa238afc249a69
ilorez <zobirnajdaoui@gmail.com>
2026-06-24 15:51:13 +0100

    update ex02

--- CPP_Module_09/ex02/main.cpp
+++ CPP_Module_09/ex02/main.cpp
@@ -1,26 +1,31 @@
 #include "includes/PmergeMe.hpp"
 #include <cstdlib>
+#include <deque>
 #include <exception>
 #include <iostream>
+#include <vector>
 
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
+  if (argc < 2)
   {
     std::cerr << "Error: require positive integer sequence as an argumen!" << std::endl;
     return (EXIT_FAILURE);
   }
   try {
-    PmergeMe re;
-    re.validateInput(argv[1]);
-    re.createPairList();
-    re.sortPairs();
-    re.setupChains();
-    re.generateJacobsthalSq();
-    re.insertPends();
-	re.displayInfo();
-    
+    PmergeMe<std::vector> vec;
+	vec.sort(argv);
+
+    PmergeMe<std::deque> deq;
+	deq.sort(argv);
+
+	// Display Info
+	vec.displayInput();
+	vec.displayMainChain();
+	vec.displayInterval("std::vector");
+	deq.displayInterval("std::deque");
+
 	// DEBUG
     //re.printAll();
   } catch (std::exception &e) {
